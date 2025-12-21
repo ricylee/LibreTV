cbf4601d3b55d1ad4e2699dba6cd322d2a34a4ee
ricylee <167521793+ricylee@users.noreply.github.com>
2025-12-21 10:19:08 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -25,7 +25,6 @@ const API_SITES = {
     dyttzy: {
         api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
         name: '电影天堂资源',
-        detail: 'http://caiji.dyttzyapi.com', 
     },
     ruyi: {
         api: 'https://cj.rycjapi.com/api.php/provide/vod',
